19d8d9fb174ff28e2e2dbfcbf0e18c685da65dbe
Johan Bjäreholt <johan@bjareho.lt>
2019-11-02 15:43:28 +0100

    build.rs: create src/protocols if it doesn't exist

--- src/build.rs
+++ src/build.rs
@@ -1,27 +1,25 @@
 extern crate wayland_scanner;
 
-use std::path::Path;
-
+use std::fs::create_dir_all;
 use wayland_scanner::{Side, generate_code};
 
 fn main() {
     // Location of the xml file, relative to the `Cargo.toml`
-
-    // Target directory for the generate files
-    let out_dir = Path::new("./src/protocols");
-
     // (xmlsrc, outsrc)
     let protocols = vec!(
-        ("protocols/wlr-foreign-toplevel-management-unstable-v1.xml",
-         "wlr-foreign-toplevel-management.rs"),
-        ("protocols/idle.xml",
-         "idle.rs"),
+        ("./protocols/wlr-foreign-toplevel-management-unstable-v1.xml",
+         "./src/protocols/wlr-foreign-toplevel-management.rs"),
+        ("./protocols/idle.xml",
+         "./src/protocols/idle.rs"),
     );
+
+    // Create "./src/protocols" folder for generated bindings
+    create_dir_all("./src/protocols").unwrap();
 
     for protocol in protocols {
         let (xmlsrc, outsrc) = protocol;
         generate_code(
-            xmlsrc, out_dir.join(outsrc),
+            xmlsrc, outsrc,
             Side::Client, // Replace by `Side::Server` for server-side code
         );
     }
